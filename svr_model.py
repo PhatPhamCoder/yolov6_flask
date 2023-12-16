d9accd51e5aa92c4a0e5bd1b6b19b46ad554eaac
PhatPham <phamhoangminhphat@gmail.com>
2023-12-16 17:19:42 +0700

    update code

--- svr_model.py
+++ svr_model.py
@@ -1,33 +1,67 @@
+import shutil
 import pickle
 from flask import Flask, render_template, request
 import os
 from random import random
 from my_yolov6 import my_yolov6
 import cv2
+from utils import *\
 
 yolov6_model = my_yolov6("weights/mass_detect.pt","cpu","data/data.yaml", 640, True)
 
 # Khởi tạo Flask
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "static"
+app.config['ALLOWED_EXTENSIONS'] = ['.dicom']
 
 # Hàm xử lý request
 @app.route("/", methods=['GET', 'POST'])
 def home_page():
     # Nếu là POST (gửi file)
     if request.method == "POST":
-         try:
-            # Lấy file gửi lên
-            image = request.files['file']
+        try:
+            # Create a new folder to store uploaded dicom files
+            # if os.path.isdir('static') == False:
+            #     uploads = 'static'
+            #     os.mkdir(uploads)
+            
+            if not os.path.exists('static'):
+                os.mkdir('static')
+
+            # if not os.path.exists('static/result_images_dir'):
+            #     os.mkdir('static/result_images_dir')
+
+            # Handle DICOM file uploads
+            dicom_list = []
+            for file in request.files.getlist('file'):
+                filename = file.filename
+                file_ext = os.path.splitext(filename)[1].lower()
+                if file_ext in app.config['ALLOWED_EXTENSIONS']:
+                    dicom_list.append(filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+            output_dir = os.path.join(app.config['UPLOAD_FOLDER'])
+            print("output_dir:::",output_dir)
+            # # Convert DICOM images to PNG
+            image_size_list = convert_dicom_to_png(dicom_list,output_dir)
+            print("image_size_list:::",image_size_list[1])
+
+            image = image_size_list[1]
+            # print(image)
             if image:
                 # Lưu file
-                path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-                print("Save = ", path_to_save)
-                image.save(path_to_save)
+
+                print("image.filename::::",image)
+                path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image)
+               
+                print("path_to_save:::",path_to_save)
+
+                # image.save(path_to_save)
 
                 # Convert image to dest size tensor
                 frame = cv2.imread(path_to_save)
-
+                print(frame)
                 frame, ndet = yolov6_model.infer(frame, conf_thres=0.6, iou_thres=0.45)
                 print(ndet)
                 if ndet!=0:
@@ -35,17 +69,17 @@ def home_page():
                 
                     # Trả về kết quả
                     print("Tải file lên thành công")
-                    return render_template("index.html", user_image = image.filename , rand = str(random()),
+                    return render_template("index.html", user_image = image , rand = str(random()),
                                            msg="Tải file lên thành công", ndet = ndet)
                 else:
                     print("Không nhận diện được vật thể")
-                    return render_template("index.html", user_image = image.filename , rand = str(random()),
+                    return render_template("index.html", user_image = image , rand = str(random()),
                                            msg="Không nhận diện được vật thể", ndet = ndet)
             else:
                 # Nếu không có file thì yêu cầu tải file
                 return render_template('index.html', msg='Hãy chọn file để tải lên')
 
-         except Exception as ex:
+        except Exception as ex:
             # Nếu lỗi thì thông báo
             print(ex)
             return render_template('index.html', msg='Không nhận diện được vật thể')
---
+++ utils.py
@@ -0,0 +1,47 @@
+from PIL import Image
+import pydicom
+import os
+import numpy as np
+from pydicom.pixel_data_handlers.util import apply_voi_lut
+
+def read_xray(path, voi_lut = True, fix_monochrome = True):
+    # Original from: https://www.kaggle.com/raddar/convert-dicom-to-np-array-the-correct-way
+    dicom = pydicom.read_file(path)
+    
+    # VOI LUT (if available by DICOM device) is used to transform raw DICOM data to 
+    # "human-friendly" view
+    if voi_lut:
+        data = apply_voi_lut(dicom.pixel_array, dicom)
+    else:
+        data = dicom.pixel_array
+               
+    # depending on this value, X-ray may look inverted - fix that:
+    if fix_monochrome and dicom.PhotometricInterpretation == "MONOCHROME1":
+        data = np.amax(data) - data
+        
+    data = data - np.min(data)
+    data = data / np.max(data)
+    data = (data * 255).astype(np.uint8)
+        
+    return data
+
+def convert_dicom_to_png(dicom_file_list, output_dir):
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for dicom_filename in dicom_file_list:
+        dicom_path = os.path.join('static', dicom_filename)
+
+        # Read the DICOM file
+        image = read_xray(dicom_path, voi_lut=True, fix_monochrome=True)
+
+        # Convert NumPy array to PIL Image
+        image = Image.fromarray(image)
+
+        # Save the image as PNG
+        png_filename = os.path.splitext(dicom_filename)[0] + '.jpg'
+        png_path = os.path.join(output_dir, png_filename)
+        image.save(png_path)
+
+    return os.listdir(output_dir)  # Return a list of saved PNG file names
